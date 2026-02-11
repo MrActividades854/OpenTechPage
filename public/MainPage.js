8fdc4377053c4c7b8eb72c4ec6100aae80251882
Jerg7 <juanestebanrosero0811@gmail.com>
2026-02-11 18:11:03 -0500

    aaeerr

--- public/MainPage.js
+++ public/MainPage.js
@@ -1,11 +1,11 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
+new Swiper('.proyects-wrapper', {
     loop: true,
+    spaceBetween: 50,
   
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
   
     // Navigation arrows
@@ -13,9 +13,16 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      },
+      768: {
+        slidesPerView: 2
+      },
+      1024: {
+        slidesPerView: 2
+      }
+    }
   });
